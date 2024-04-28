530973a7ab928da0006d8f77c0e66bc39d97deb5
AstralScrub <vongandrew5@gmail.com>
2024-04-27 17:20:50 -0700

    moved code around to better fit the assignment requirements & implemented a redimentary "help"

--- src/inspect.c
+++ src/inspect.c
@@ -4,19 +4,23 @@
 #include <errno.h>
 #include <string.h>
 
+#define PLAIN_TEXT  (0)
+#define JSON        (1)
+
+struct stat fileInfo;
+char * fileName;
+int format;
+
+
+void argsHandler(int argc, char * argv[]);
+void help(int argc, char * argv[]);
+
 int main(int argc, char *argv[]) {
-    struct stat fileInfo;
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
-        return 1;
-    }
-
-    if (stat(argv[1], &fileInfo) != 0) {
-        fprintf(stderr, "Error getting file info for %s: %s\n", argv[1], strerror(errno));
-        return 1;
-    }
-
-    printf("Information for %s:\n", argv[1]);
+    
+    
+    argsHandler(argc, argv);
+    
+    printf("Information for %s:\n", fileName);
     printf("File Inode: %lu\n", fileInfo.st_ino);
     printf("File Type: ");
     if (S_ISREG(fileInfo.st_mode))
@@ -43,4 +47,35 @@ int main(int argc, char *argv[]) {
     printf("Last Status Change Time: %ld\n", fileInfo.st_ctime);
 
     return 0;
+}
+
+void help(int argc, char * argv[])
+{
+    printf("%s: %s [-? | -i <file_path> | -a [directory_path]] [-a | -f [text|json]] [-l]\n", argv[0], argv[0]);
+    
+    
+}
+
+void argsHandler(int argc, char * argv[])
+{    
+
+    
+    if (argc == 1)
+    {
+        help(argc, argv);
+        exit(1);
+    }
+    else if (argc == 2)
+    {
+        format = PLAIN_TEXT;
+        fileName = argv[1];
+    }
+    else
+    {
+        fileName = argv[0];
+    }
+    if (stat(fileName, &fileInfo) != 0) {
+        fprintf(stderr, "Error getting file info for %s: %s\n", argv[1], strerror(errno));
+        exit(1);
+    }
 }
