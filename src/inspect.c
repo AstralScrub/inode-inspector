e119ece48d088d6fb4b3d1f03d3c722dbc810a86
AstralScrub <vongandrew5@gmail.com>
2024-04-29 17:44:13 -0700

    added support for selection and humanReadable settings; added checks for inode file permissions

--- src/inspect.c
+++ src/inspect.c
@@ -4,13 +4,18 @@
 #include <errno.h>
 #include <string.h>
 
-#define PLAIN_TEXT  (0)
-#define JSON        (1)
+#define SELECTION_SINGLE    (0)
+#define SELECTION_ALL       (1)
+#define SELECTION_RECURSIVE (2)
+
+#define FORMAT_JSON         (0)
+#define FORMAT_TEXT         (1)
 
 struct stat fileInfo;
 char * fileName;
+int selectionMode;
+int humanReadable;
 int format;
-
 
 void argsHandler(int argc, char * argv[]);
 void help(int argc, char * argv[]);
@@ -20,8 +25,34 @@ int main(int argc, char *argv[]) {
     
     argsHandler(argc, argv);
     
+    if (stat(fileName, &fileInfo) != 0) 
+    {
+        fprintf(stderr, "Error getting file info for %s: %s\n", argv[1], strerror(errno));
+        exit(1);
+    }
+    
     printf("Information for %s:\n", fileName);
     printf("File Inode: %lu\n", fileInfo.st_ino);
+    printf("Premission: ");
+    if(S_IRUSR&fileInfo.st_mode)
+        printf("r-");
+    if(S_IWUSR&fileInfo.st_mode)
+        printf("w-");
+    if(S_IXUSR&fileInfo.st_mode)
+        printf("x-");
+    if(S_IRGRP&fileInfo.st_mode)
+        printf("r--");
+    if(S_IRGRP&fileInfo.st_mode)
+        printf("w--");
+    if(S_IRGRP&fileInfo.st_mode)
+        printf("x--");
+    if(S_IROTH&fileInfo.st_mode)
+        printf("r---");
+    if(S_IROTH&fileInfo.st_mode)
+        printf("w---");
+    if(S_IROTH&fileInfo.st_mode)
+        printf("x---");
+    printf("\n");
     printf("File Type: ");
     if (S_ISREG(fileInfo.st_mode))
         printf("regular file\n");
@@ -40,6 +71,7 @@ int main(int argc, char *argv[]) {
     else
         printf("unknown?\n");
 
+    
     printf("Number of Hard Links: %lu\n", fileInfo.st_nlink);
     printf("File Size: %lu bytes\n", fileInfo.st_size);
     printf("Last Access Time: %ld\n", fileInfo.st_atime);
@@ -52,14 +84,10 @@ int main(int argc, char *argv[]) {
 void help(int argc, char * argv[])
 {
     printf("%s: %s [-? | -i <file_path> | -a [directory_path]] [-a | -f [text|json]] [-l]\n", argv[0], argv[0]);
-    
-    
 }
 
 void argsHandler(int argc, char * argv[])
-{    
-
-    
+{
     if (argc == 1)
     {
         help(argc, argv);
@@ -67,15 +95,41 @@ void argsHandler(int argc, char * argv[])
     }
     else if (argc == 2)
     {
-        format = PLAIN_TEXT;
+        format = FORMAT_TEXT;
+        selectionMode = SELECTION_SINGLE;
         fileName = argv[1];
     }
     else
     {
+        format = -1;
+        selectionMode = -1;
         fileName = argv[0];
-    }
-    if (stat(fileName, &fileInfo) != 0) {
-        fprintf(stderr, "Error getting file info for %s: %s\n", argv[1], strerror(errno));
-        exit(1);
+        
+        for (int i = 1; i < argc; i++)
+        {
+            char * arg = argv[i];
+            if (strcmp(arg, "-i") == 0)
+            {
+                printf("i\n");
+                selectionMode = SELECTION_SINGLE;
+                if (i + 1 < argc)
+                    fileName = argv[i+1];
+            } 
+            else if (strcmp(arg, "-a") == 0)
+            {
+                printf("a\n");
+                selectionMode = SELECTION_SINGLE;
+                fileName = argv[i+1];
+
+                if (i + 2 < argc)
+                {
+                    if (strcmp(argv[i+2], "-r"))
+                    {
+                        selectionMode = SELECTION_RECURSIVE;
+                    }
+                }
+            }
+            
+        }
     }
 }
